a776cd1ec107a88b168b168993fd140a7edfbec3
Kanav Gupta <kakarrot.goku2003@gmail.com>
2023-08-14 20:25:45 +0530

    conecteed everything :)

--- Backend/app.py
+++ Backend/app.py
@@ -13,17 +13,18 @@ CORS(app)
 
 
 # Load the pre-trained emotion detection model
-json_file = open("./emotiondetector.json", "r")
+json_file = open("./Backend/emotiondetector2.json", "r")
 model_json = json_file.read()
 json_file.close()
 model = model_from_json(model_json)
-model.load_weights("./emotiondetector.h5")
+model.load_weights("./Backend/emotiondetector2.h5")
 
 # Load Haarcascade classifier XML file
 haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(haar_file)
 
 prediction_results = []
+values=[]
 
 def extract_features(image):
     feature = image.reshape(1, 48, 48, 1)
@@ -46,14 +47,34 @@ def generate_frames():
             face_image = gray[y:y+h, x:x+w]
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
             resized_face = cv2.resize(face_image, (48, 48))
-            img = extract_features(resized_face)
-            pred = model.predict(img)  
+            normalized_face = resized_face 
+            img = extract_features(normalized_face)
+            # Predict emotion and apply thresholding
+            pred = model.predict(img)
             prediction_label = labels[pred.argmax()]
-            prediction_percentage = np.max(pred)*10   # Get the maximum prediction probability
-            prediction_results.append((prediction_label, prediction_percentage))
+            prediction_percentage = np.max(pred)*10 
             
-            text = f"{prediction_label}: {prediction_percentage:.2f}%"
-            cv2.putText(frame, text, (x, y-40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            # Store prediction for smoothing
+            prediction_results.append(prediction_percentage)
+            smoothed_pred = np.mean(prediction_results)
+            values.append((prediction_label, smoothed_pred))
+            text = f"{prediction_label}: {smoothed_pred:.2f}%"
+            # Define text style
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.7
+            font_color = (153, 51, 255) 
+            font_thickness = 2
+
+            # Calculate text size to position it properly
+            text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
+            text_width, text_height = text_size
+
+            # Calculate text position above the bounding box
+            text_x = x + (w - text_width) // 2
+            text_y = y - 15  # Slightly above the top of the bounding box
+
+            # Overlay text on the frame
+            cv2.putText(frame, text, (text_x, text_y), font, font_scale, font_color, font_thickness)
         
         _, buffer = cv2.imencode('.jpg', frame)  
         frame_data = buffer.tobytes()
@@ -73,8 +94,8 @@ def test():
 @app.route('/value', methods=['GET'])
 def value():
     try:
-        if prediction_results:
-            prediction_label, prediction_percentage = prediction_results[-1]
+        if values:
+            prediction_label, prediction_percentage = values[-1]
             value = {"label": prediction_label, "percentage": prediction_percentage}
             return jsonify(value)
         # else:
--- Model/hacathon code.py
+++ Model/hacathon code.py
@@ -4,11 +4,11 @@ from keras.models import model_from_json
 from keras.models import  load_model
 # Load the pre-trained emotion detection model
 model_path = "C:/projects/MNIT Hackathon/Model/"
-json_file = open(model_path + "emotiondetector.json", "r")
+json_file = open(model_path + "emotiondetector2.json", "r")
 model_json = json_file.read()
 json_file.close()
 model = model_from_json(model_json)
-model.load_weights(model_path + "emotiondetector.h5")
+model.load_weights(model_path + "emotiondetector2.h5")
 
 
 # Load Haarcascade classifier XML file
@@ -36,13 +36,34 @@ while True:
             face_image = gray[y:y+h, x:x+w]
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
             resized_face = cv2.resize(face_image, (48, 48))
-            img = extract_features(resized_face)
-            pred = model.predict(img)  
+            normalized_face = resized_face 
+            img = extract_features(normalized_face)
+            # Predict emotion and apply thresholding
+            pred = model.predict(img)
             prediction_label = labels[pred.argmax()]
-            prediction_percentage = np.max(pred)*10   # Get the maximum prediction probability
-            prediction_results.append((prediction_label, prediction_percentage))
-            text = f"{prediction_label}: {prediction_percentage:.2f}%"
-            cv2.putText(frame, text, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            prediction_percentage = np.max(pred)*10 
+            
+            # Store prediction for smoothing
+            prediction_results.append(prediction_percentage)
+            smoothed_pred = np.mean(prediction_results)
+            
+            text = f"{prediction_label}: {smoothed_pred:.2f}%"
+            # Define text style
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.7
+            font_color = (153, 51, 255) 
+            font_thickness = 2
+
+            # Calculate text size to position it properly
+            text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
+            text_width, text_height = text_size
+
+            # Calculate text position above the bounding box
+            text_x = x + (w - text_width) // 2
+            text_y = y - 15  # Slightly above the top of the bounding box
+
+            # Overlay text on the frame
+            cv2.putText(frame, text, (text_x, text_y), font, font_scale, font_color, font_thickness)
         
         cv2.imshow("Emotion Detection", frame)
         
