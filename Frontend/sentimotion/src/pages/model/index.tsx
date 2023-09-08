04cf904202d24cca7e8946ac1aac4c05eac7c735
Kanav Gupta <kakarrot.goku2003@gmail.com>
2023-09-08 08:42:10 +0530

    changes

--- Frontend/sentimotion/src/pages/model/index.tsx
+++ Frontend/sentimotion/src/pages/model/index.tsx
@@ -65,7 +65,7 @@ const Model = () => {
                 display: 'flex',
               }}
             >
-              Models
+              Features
             </Typography>
           </div>)}
       </StyledGrid>
